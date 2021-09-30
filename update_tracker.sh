8ab92c61bb51893aa72ffbdda1208fed5d7a67cc
MCUmbrella <mcumbrella@protonmail.com>
2021-09-30 19:03:53 +0800

    a

---
+++ update_tracker.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# This script is used to apply the newest tracker list to all torrents
+# By: MCUmbrella
+
+echo "STEP1: create temp files"
+echo > tmp_tracker.txt && echo > tmp_tracker.txt.new && echo "[OK] STEP1 completed"
+
+echo "STEP2: get tracker list file from trackerlist.com"
+wget "https://trackerslist.com/all.txt" -O tmp_tracker.txt &> /dev/null && echo "[OK] STEP2 completed"
+
+echo "STEP3: process original list file for further use"
+tr -s '\n' '\n' < tmp_tracker.txt > tmp_tracker.txt.new
+echo >> tmp_tracker.txt.new && echo "[OK] STEP3 completed"
+
+echo "STEP4: add tracker(s) to all torrents"
+a=0
+for t in $(cat tmp_tracker.txt.new)
+do
+	transmission-edit --add $t /var/lib/transmission/.config/transmission-daemon/torrents/*.torrent > /dev/null
+	echo "Added tracker $t to all torrents"
+	a=$(expr $a + 1)
+done && echo "[OK] STEP4 completed"
+
+echo "STEP5: delete temp files"
+rm -f tmp_tracker.txt* && echo "[OK] STEP5 completed"
+echo "[OK] $a tracker(s) added"
