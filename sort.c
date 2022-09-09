d661fa65e471a97658bf901b8bd87539418cfc5a
MCUmbrella <mcumbrella@protonmail.com>
2022-09-09 17:42:40 +0800

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,18 @@
+int* sort(int* nums, int numsLen)
+{
+	int sorted=0;
+	int tmp=0;
+	do
+		for(int i=0; i!=numsLen-1; i++)
+			if(nums[i] <= nums[i+1])
+				sorted++;
+			else
+			{
+				tmp=nums[i+1];
+				nums[i+1]=nums[i];
+				nums[i]=tmp;
+				sorted=0;
+			}
+	while(sorted<=numsLen);
+	return nums;
+}
