187af78dfdc7965d3625fcdf087c3c27cac03054
MCUmbrella <mcumbrella@protonmail.com>
2023-06-24 23:03:54 +0800

    Update Synthesizer.java

--- Synthesizer.java
+++ Synthesizer.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+import java.util.*;
 
 public class Synthesizer
 {
@@ -6,6 +6,7 @@ public class Synthesizer
     {
         return synthesize(src, pattern.toCharArray());
     }
+
     public static String desynthesize(String src, String pattern)
     {
         return desynthesize(src, pattern.toCharArray());
@@ -13,28 +14,34 @@ public class Synthesizer
 
     public static String synthesize(String src, char[] pattern)
     {
-        HashSet<Character> set=new HashSet<>();
-        for(char c : pattern) set.add(c);
-        StringBuilder sb=new StringBuilder();
-        for(int i=0; i!=src.length(); i++)
-            if(!set.contains(src.charAt(i))) sb.append(src.charAt(i));
+        Set<Character> set = new HashSet<>();
+        for(char c : pattern)
+            set.add(c);
+        StringBuilder sb = new StringBuilder(src.length());
+        for(int i = 0; i != src.length(); i++)
+            if(!set.contains(src.charAt(i)))
+                 sb.append(src.charAt(i));
         return sb.toString();
     }
+
     public static String desynthesize(String src, char[] pattern)
     {
-        HashSet<Character> set=new HashSet<>();
-        for(char c : pattern) set.add(c);
-        StringBuilder sb=new StringBuilder();
-        for(int i=0; i!=src.length(); i++)
-            if(set.contains(src.charAt(i))) sb.append(src.charAt(i));
+        Set<Character> set = new HashSet<>();
+        for(char c : pattern)
+            set.add(c);
+        StringBuilder sb = new StringBuilder(src.length());
+        for(int i = 0; i != src.length(); i++)
+            if(set.contains(src.charAt(i)))
+                sb.append(src.charAt(i));
         return sb.toString();
     }
 
     public static void main(String[] args)
     {
-        String test="my name is van. im an artist. im a performance artist. im hired for people to fulfill their fantasies, their deep dark fantasies.";
-        System.out.println(test);
-        System.out.println(synthesize(test, "hired"));
-        System.out.println(desynthesize(test, "hired"));
+        Scanner sc = new Scanner(System.in);
+        String src = sc.nextLine();
+        String pattern = sc.nextLine();
+        System.out.println(synthesize(src, pattern));
+        System.out.println(desynthesize(src, pattern));
     }
 }
